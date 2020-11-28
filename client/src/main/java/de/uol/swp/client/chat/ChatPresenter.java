1cd14fca6ca54225e64772aaa28fccb0efc3f1c6
Timo <timo.gerken@uni-oldenburg.de>
2020-11-28 14:09:36 +0100

    ChatPresenter und ChatView Klasse erstellt. Noch Probleme beim Laden der Chat Scene.

---
+++ client/src/main/java/de/uol/swp/client/chat/ChatPresenter.java
@@ -0,0 +1,8 @@
+package de.uol.swp.client.chat;
+
+import de.uol.swp.client.AbstractPresenter;
+
+public class ChatPresenter extends AbstractPresenter {
+
+    public static final String fxml = "/fxml/ChatView.fxml";
+}
