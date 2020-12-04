f2cb438955cd8cd20984d40021aec5c83a122cd7
Steven <steven.luong@uni-oldenburg.de>
2020-12-04 19:47:11 +0100

    Aufgabe bearbeitet: ChangePasswordExceptionMessage, ChangePasswordSuccessfulResponse und unter UserService im Server eine onChangePasswordRequest erstellt

--- server/src/main/java/de/uol/swp/server/usermanagement/UserService.java
+++ server/src/main/java/de/uol/swp/server/usermanagement/UserService.java
@@ -8,10 +8,13 @@ import de.uol.swp.common.message.ResponseMessage;
 import de.uol.swp.common.user.User;
 import de.uol.swp.common.user.exception.RegistrationExceptionMessage;
 import de.uol.swp.common.user.exception.UserDeletionExceptionMessage;
+import de.uol.swp.common.user.exception.ChangePasswordExceptionMessage;
 import de.uol.swp.common.user.request.DeleteUserRequest;
 import de.uol.swp.common.user.request.RegisterUserRequest;
+import de.uol.swp.common.user.request.UpdateUserRequest;
 import de.uol.swp.common.user.response.RegistrationSuccessfulResponse;
 import de.uol.swp.common.user.response.UserDeletionSuccessfulResponse;
+import de.uol.swp.common.user.response.ChangePasswordSuccessfulResponse;
 import de.uol.swp.server.AbstractService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -111,5 +114,34 @@ public class UserService extends AbstractService {
             returnMessage.setMessageContext(msg.getMessageContext().get());
         }
     }
-
+    /**
+     * Handles ChangePasswordRequest found on the EventBus
+     *
+     * If a ChangePasswordRequest is detected on the EventBus, this method is called.
+     * It tries to change the Password of an User via the UserManagement.
+     * If this succeeds, a ChangePasswordSuccessfulResponse is posted on the EventBus,
+     * otherwise a ChangePasswordExceptionMessage gets posted there.
+     *
+     * @param msg The ChangePasswordRequest found on the Eventbus
+     * @author Eric Vuong, Steven Luong
+     * @since 2020-12-03
+     *
+     */
+    @Subscribe
+    private void onChangePasswordRequest(UpdateUserRequest msg) {
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Got new ChangePassword message with " + msg.getUser());
+        }
+        ResponseMessage returnMessage;
+        try {
+            userManagement.updateUser(msg.getUser());
+            returnMessage = new ChangePasswordSuccessfulResponse();
+        } catch (Exception e) {
+            LOG.error(e);
+            returnMessage = new ChangePasswordExceptionMessage("Cannot change Password" + msg.getUser() + " " + e.getMessage());
+        }
+        if (msg.getMessageContext().isPresent()) {
+            returnMessage.setMessageContext(msg.getMessageContext().get());
+        }
+    }
 }
