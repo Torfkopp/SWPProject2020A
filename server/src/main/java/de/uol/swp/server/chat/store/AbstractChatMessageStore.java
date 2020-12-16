998785ddcd2b3bfe325040a75e7f4e0adf562e7c
Temmo Junkhoff <temmo.junkhoff@uni-oldenburg.de>
2020-12-16 22:51:17 +0100

    ChatMessageStore (Interface, Abstract, MainMemoryBased) erstellt
    Co-authored-by: Phillip-André Suhr <phillip-andre.suhr@uni-oldenburg.de>

---
+++ server/src/main/java/de/uol/swp/server/chat/store/AbstractChatMessageStore.java
@@ -0,0 +1,12 @@
+package de.uol.swp.server.chat.store;
+
+/**
+ * Base class for all kinds of different ChatMessageStores
+ *
+ * @author Temmo Junkhoff
+ * @see ChatMessageStore
+ * @see MainMemoryBasedChatMessageStore
+ * @since 2020-12-16
+ */
+public abstract class AbstractChatMessageStore implements ChatMessageStore {
+}
---
+++ server/src/main/java/de/uol/swp/server/chat/store/MainMemoryBasedChatMessageStore.java
@@ -0,0 +1,74 @@
+package de.uol.swp.server.chat.store;
+
+import de.uol.swp.common.chat.ChatMessage;
+import de.uol.swp.common.chat.dto.ChatMessageDTO;
+import de.uol.swp.common.user.User;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * This is a ChatMessage store.
+ * <p>
+ * This ChatMessage store only resides in the RAM of your computer
+ * and only for as long as the server is running. Therefore the messages have to be
+ * added every time the server is started.
+ *
+ * @author Temmo Junkhoff
+ * @author Phillip-André Suhr
+ * @see ChatMessageStore
+ * @see AbstractChatMessageStore
+ * @since 2020-12-16
+ */
+public class MainMemoryBasedChatMessageStore extends AbstractChatMessageStore implements ChatMessageStore {
+
+    private static final int MAX_HISTORY = 10000;
+    private static int id_count = 0;
+    // LinkedHashMaps keep insertion order and remove the eldest element when the override below returns true
+    private final Map<Integer, ChatMessage> chatHistory = new LinkedHashMap<>() {
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<Integer, ChatMessage> eldest) {
+            return size() > MAX_HISTORY;
+        }
+    };
+
+    @Override
+    public Optional<ChatMessage> findMessage(int id) {
+        ChatMessage chatMessage = chatHistory.get(id);
+        if (chatMessage != null) {
+            return Optional.of(chatMessage);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<ChatMessage> getLatestMessages(int amount) {
+        List<Map.Entry<Integer, ChatMessage>> list = new LinkedList<>(chatHistory.entrySet());
+        Collections.reverse(list);
+        return list.stream().limit(amount).map(Map.Entry::getValue).collect(Collectors.toList());
+    }
+
+    @Override
+    public ChatMessage createChatMessage(User author, String content) {
+        if (author == null) {
+            throw new IllegalArgumentException("Message author must not be null");
+        }
+        id_count += 1;
+        ChatMessage chatMessage = new ChatMessageDTO(id_count, author, content);
+        chatHistory.put(id_count, chatMessage);
+        return chatMessage;
+    }
+
+    @Override
+    public ChatMessage updateChatMessage(int id, String updatedContent) {
+        ChatMessage messageToEdit = chatHistory.get(id);
+        messageToEdit.setContent(updatedContent);
+        return messageToEdit;
+    }
+
+    @Override
+    public void removeChatMessage(int id) {
+        chatHistory.remove(id);
+        // TODO: send Message to client
+    }
+}
