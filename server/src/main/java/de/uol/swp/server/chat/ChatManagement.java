8839d81986e493ce8b2fe92a9d9863608322d62a
Phillip-André Suhr <phillip-andre.suhr@uni-oldenburg.de>
2020-12-17 01:48:27 +0100

    ChatManagement (Interface, Abstract & Implementierung) sowie Exception erstellt und dokumentiert
    Co-authored-by: Temmo Junkhoff <temmo.junkhoff@uni-oldenburg.de>

---
+++ server/src/main/java/de/uol/swp/server/chat/ChatManagement.java
@@ -0,0 +1,71 @@
+package de.uol.swp.server.chat;
+
+import com.google.common.base.Strings;
+import com.google.inject.Inject;
+import de.uol.swp.common.chat.ChatMessage;
+import de.uol.swp.common.user.User;
+import de.uol.swp.server.chat.store.ChatMessageStore;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * The type Chat management.
+ *
+ * @author Temmo Junkhoff
+ * @author Phillip-André Suhr
+ * @see AbstractChatManagement
+ * @see IChatManagement
+ * @see ChatMessageStore
+ * @see User
+ * @since 2020-12-16
+ */
+public class ChatManagement extends AbstractChatManagement implements IChatManagement {
+
+    private final ChatMessageStore chatMessageStore;
+
+    /**
+     * Constructor
+     *
+     * @param chatMessageStore object of the ChatMessageStore to be used
+     * @see ChatMessageStore
+     * @since 2020-12-16
+     */
+    @Inject
+    public ChatManagement(ChatMessageStore chatMessageStore) {
+        this.chatMessageStore = chatMessageStore;
+    }
+
+    @Override
+    public List<ChatMessage> getLatestMessages(int amount) {
+        return chatMessageStore.getLatestMessages(amount);
+    }
+
+    @Override
+    public ChatMessage createChatMessage(User author, String content) {
+        if (Strings.isNullOrEmpty(content)) {
+            throw new ChatManagementException("Content must not be empty");
+        } else if (author == null) {
+            throw new ChatManagementException("Author must not be null");
+        }
+        return chatMessageStore.createChatMessage(author, content);
+    }
+
+    @Override
+    public ChatMessage updateChatMessage(int id, String updatedContent) {
+        Optional<ChatMessage> chatMessage = chatMessageStore.findMessage(id);
+        if (chatMessage.isEmpty()) {
+            throw new ChatManagementException("ChatMessage ID unknown");
+        }
+        return chatMessageStore.updateChatMessage(id, updatedContent);
+    }
+
+    @Override
+    public void dropChatMessage(int id) {
+        Optional<ChatMessage> chatMessage = chatMessageStore.findMessage(id);
+        if (chatMessage.isEmpty()) {
+            throw new ChatManagementException("ChatMessage ID unknown");
+        }
+        chatMessageStore.removeChatMessage(id);
+    }
+}
---
+++ server/src/main/java/de/uol/swp/server/chat/ChatManagementException.java
@@ -0,0 +1,26 @@
+package de.uol.swp.server.chat;
+
+/**
+ * Exception thrown in ChatManagement
+ * <p>
+ * This exception is thrown if someone wants to create a ChatMessage without author or content
+ * or someone tries to modify or delete a ChatMessage that does not (yet)
+ * exist within the ChatMessageStore.
+ *
+ * @author Temmo Junkhoff
+ * @author Phillip-André Suhr
+ * @see ChatManagement
+ * @since 2020-12-16
+ */
+public class ChatManagementException extends RuntimeException {
+
+    /**
+     * Constructor
+     *
+     * @param s String containing the cause for the exception.
+     * @since 2020-12-16
+     */
+    ChatManagementException(String s) {
+        super(s);
+    }
+}
