75253b502a418d0fa01dfd53f40088a84d7a86d1
Marvin Drees <marvin@ceres-sys.de>
2020-12-21 18:46:30 +0100

    Kommentar ist nun ausführlicher

--- common/src/main/java/de/uol/swp/common/lobby/message/LobbyExceptionMessage.java
+++ common/src/main/java/de/uol/swp/common/lobby/message/LobbyExceptionMessage.java
@@ -4,7 +4,13 @@ import de.uol.swp.common.message.ExceptionMessage;
 import de.uol.swp.common.message.ResponseMessage;
 
 /**
- * Message sent by the server when the user can't join a specific lobby
+ * This class returns exceptions to the client caused by various
+ * serverside lobby related errors
+ *
+ * @see de.uol.swp.common.message.ResponseMessage
+ * @see de.uol.swp.common.message.ExceptionMessage
+ * @author Marvin
+ * @since 2020-12-19
  */
 
 public class LobbyExceptionMessage extends ExceptionMessage implements ResponseMessage {
