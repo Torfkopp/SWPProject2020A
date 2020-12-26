85ff6013ecf7e57d1dea396484d58e3a1a041ac5
Eric Vuong <eric.vuong@uni-oldenburg.de>
2020-12-26 20:48:11 +0100

    UML-Modelle aktualisiert
    Modelle der Module Client, Common und Server aktualisiert, PNGs exportiert
    Vorgang SWP2020A-75 erledigt
    
    - LobbyExceptionMessage: Redundante Interfaceimplementierung entfernt für klarere Vererbungsstruktur im UML-Modell
    
    Co-authored-by: Phillip-André Suhr <phillip-andre.suhr@uni-oldenburg.de>

--- common/src/main/java/de/uol/swp/common/lobby/message/LobbyExceptionMessage.java
+++ common/src/main/java/de/uol/swp/common/lobby/message/LobbyExceptionMessage.java
@@ -1,19 +1,17 @@
 package de.uol.swp.common.lobby.message;
 
 import de.uol.swp.common.message.ExceptionMessage;
-import de.uol.swp.common.message.ResponseMessage;
 
 /**
  * This class returns exceptions to the client caused by various
  * serverside lobby related errors
  *
  * @author Marvin
- * @see de.uol.swp.common.message.ResponseMessage
  * @see de.uol.swp.common.message.ExceptionMessage
  * @since 2020-12-19
  */
 
-public class LobbyExceptionMessage extends ExceptionMessage implements ResponseMessage {
+public class LobbyExceptionMessage extends ExceptionMessage {
 
     /**
      * Constructor
