b4cbff350dd41bf01997dcfed66715f5aba9a828
Temmo Junkhoff <temmo.junkhoff@uol.de>
2021-01-02 19:01:42 +0100

    Doku angefangen
    Co-authored-by: Phillip-André Suhr <phillip-andre.suhr@uni-oldenburg.de>

--- client/src/main/java/de/uol/swp/client/chat/IChatService.java
+++ client/src/main/java/de/uol/swp/client/chat/IChatService.java
@@ -17,11 +17,20 @@ public interface IChatService {
     /**
      * Method to create a new ChatMessage
      *
-     * @param msg The contents of the new ChatMessage
+     * @param author The User who authored the ChatMessage
+     * @param msg    The contents of the new ChatMessage
      * @since 2020-12-17
      */
     void newMessage(User author, String msg);
 
+    /**
+     * Method to create a new ChatMessage in a lobbyChat
+     *
+     * @param author      The User who authored the ChatMessage
+     * @param msg         The contents of the new ChatMessage
+     * @param originLobby The name of the Lobby where the ChatMessage was sent
+     * @since 2021-01-02
+     */
     void newMessage(User author, String msg, String originLobby);
 
     /**
@@ -32,6 +41,13 @@ public interface IChatService {
      */
     void deleteMessage(int id);
 
+    /**
+     * Method to delete a ChatMessage in a lobbyChat
+     *
+     * @param id          The ID of the ChatMessage to delete
+     * @param originLobby The name of the Lobby where the ChatMessage was sent
+     * @since 2021-01-02
+     */
     void deleteMessage(int id, String originLobby);
 
     /**
@@ -43,6 +59,14 @@ public interface IChatService {
      */
     void editMessage(int id, String newContent);
 
+    /**
+     * Method to change the content of a ChatMessage in a lobbyChat
+     *
+     * @param id          The ID of the ChatMessage to edit
+     * @param newContent  The new content of the ChatMessage
+     * @param originLobby The name of the Lobby where the ChatMessage was sent
+     * @since 2021-01-02
+     */
     void editMessage(int id, String newContent, String originLobby);
 
     /**
@@ -54,5 +78,13 @@ public interface IChatService {
      */
     void askLatestMessages(int amount);
 
+    /**
+     * Ask for a List of {@literal <amount>} or less ChatMessages that represents the newest {@literal <amount>}
+     * of ChatMessages
+     *
+     * @param amount      The maximum amount of ChatMessages the Client wants to request
+     * @param originLobby The name of the Lobby where the ChatMessages were sent
+     * @since 2021-01-02
+     */
     void askLatestMessages(int amount, String originLobby);
 }
