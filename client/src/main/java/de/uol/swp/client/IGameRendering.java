488507a0a2fef22da29ceae2202b58f1d8878e53
Temmo Junkhoff <tembell720@gmail.com>
2021-01-29 13:16:01 +0100

    Aufgeräumt Spielfeld-Rendering: Es wurden einige Kommentare hinzugefügt und Methoden aufgelöst.

--- client/src/main/java/de/uol/swp/client/IGameRendering.java
+++ client/src/main/java/de/uol/swp/client/IGameRendering.java
@@ -48,12 +48,11 @@ public interface IGameRendering {
         //double currentY = hexHeight / 2.0;
         for (int y = 0; y < hexes.length; y++) {
             currentX = startX;
-            if (hexes[y].length % 2 == 0) {
-                //Even Row
+            //Set the indentation for the current row of hex tiles
+            if (hexes[y].length % 2 == 0) { //Row with an even amount of hex tiles
                 currentX += hexWidth / 2.0;
                 currentX += ((maxHexesInRow - 1 - hexes[y].length) / 2.0) * hexWidth;
-            } else {
-                //Odd Row
+            } else {//Row with an odd amount of hex tiles
                 currentX += ((maxHexesInRow - hexes[y].length) / 2.0) * hexWidth;
             }
             for (int x = 0; x < hexes[y].length; x++) {
@@ -69,8 +68,8 @@ public interface IGameRendering {
                                    double settlementSize, double citySize, GraphicsContext mapCtx) {
         double currentX = startX;
         double currentY = 0;
-        int shorterRowFirst = 1;
         currentY = hexHeight * (3.0 / 4.0);
+        //For loop for the intersections in the top half of the map
         for (int y = 0; y < 3; y++) {
             double rowStartX = ((maxIntersectionsInRow - intersections[y].length) / 4.0) * hexWidth;
             currentX = rowStartX + hexWidth + startX + hexWidth / 2.0;
@@ -88,6 +87,7 @@ public interface IGameRendering {
             currentY += hexHeight / 2.0;
         }
 
+        //For loop for the intersections in the bottom half of the map
         for (int y = 3; y < intersections.length; y++) {
             double rowStartX = ((maxIntersectionsInRow - intersections[y].length) / 4.0) * hexWidth;
             currentX = rowStartX + hexWidth;
@@ -109,17 +109,17 @@ public interface IGameRendering {
     private void renderIntersection(double x, double y, IIntersection intersection,
                                     double settlementSize, double citySize, GraphicsContext mapCtx) {
         String state = intersection.getState();
-        System.out.println(state);
-        if (state.equals("f")) { //Free
-            //Nothing to do here
-        } else if (state.equals("b")) { //Blocked
-            //Maybe render something
-        } else if (state.endsWith("s")) { //Settlement
+        if (state.equals("f")) { //Free intersection
+            //Free intersections don't need to be marked, but it could easily be added here
+        } else if (state.equals("b")) { //Blocked intersection
+            //Blocked intersections don't need to be marked, but it could easily be added here
+        } else if (state.endsWith("s")) { //Intersection with settlement
             mapCtx.setFill(setColor(state));
-            drawSettlement(x, y, settlementSize, mapCtx);
-        } else if (state.endsWith("c")) { //City
+            mapCtx.fillOval(x - (settlementSize / 2.0), y - (settlementSize / 2.0), settlementSize, settlementSize);
+        } else if (state.endsWith("c")) { //Intersection with city
             mapCtx.setFill(setColor(state));
-            drawCity(x, y, citySize, mapCtx);
+            mapCtx.fillRoundRect(x - (citySize / 2.0), y - (citySize / 2.0), citySize, citySize,
+                    citySize / 2.0, citySize / 2.0);
         }
     }
 
@@ -137,46 +137,30 @@ public interface IGameRendering {
     }
 
     private void renderHex(double x, double y, IGameHex hex, double hexWidth, double hexHeight, GraphicsContext mapCtx) {
-        mapCtx.setStroke(Color.BLACK);
-        mapCtx.setLineWidth(2);
         if (hex instanceof IWaterHex) {
             mapCtx.setFill(Color.BLUE);
-            Hexagon(x, y, hexWidth, hexHeight, mapCtx);
         } else if (hex instanceof DesertHex) {
             mapCtx.setFill(Color.WHITE);
-            Hexagon(x, y, hexWidth, hexHeight, mapCtx);
         } else if (hex instanceof IResourceHex && ((IResourceHex) hex).getResource() == IResourceHex.resource.Hills) {
             mapCtx.setFill(Color.DARKORANGE);
-            Hexagon(x, y, hexWidth, hexHeight, mapCtx);
         } else if (hex instanceof IResourceHex && ((IResourceHex) hex).getResource() == IResourceHex.resource.Forest) {
             mapCtx.setFill(Color.DARKGREEN);
-            Hexagon(x, y, hexWidth, hexHeight, mapCtx);
         } else if (hex instanceof IResourceHex && ((IResourceHex) hex).getResource() == IResourceHex.resource.Mountains) {
             mapCtx.setFill(Color.DARKGREY);
-            Hexagon(x, y, hexWidth, hexHeight, mapCtx);
         } else if (hex instanceof IResourceHex && ((IResourceHex) hex).getResource() == IResourceHex.resource.Fields) {
             mapCtx.setFill(Color.YELLOW);
-            Hexagon(x, y, hexWidth, hexHeight, mapCtx);
         } else if (hex instanceof IResourceHex && ((IResourceHex) hex).getResource() == IResourceHex.resource.Pasture) {
             mapCtx.setFill(Color.LIGHTGREEN);
-            Hexagon(x, y, hexWidth, hexHeight, mapCtx);
+        } else {
+            return;
         }
-    }
-
-    private void Hexagon(double x, double y, double hexWidth, double hexHeight, GraphicsContext mapCtx) {
+        //Draw a hexagon in the set Color with a black 2px border around it
         double[] xCords = {x, x + hexWidth / 2, x + hexWidth, x + hexWidth, x + hexWidth / 2, x};
         double[] yCords = {y + (hexHeight / 4), y, y + (hexHeight / 4), y + (hexHeight / 4) * 3,
                 y + hexHeight, y + (hexHeight / 4) * 3};
         mapCtx.fillPolygon(xCords, yCords, 6);
+        mapCtx.setStroke(Color.BLACK);
+        mapCtx.setLineWidth(2);
         mapCtx.strokePolygon(xCords, yCords, 6);
-    }
-
-    private void drawSettlement(double x, double y, double settlementSize, GraphicsContext mapCtx) {
-        mapCtx.fillOval(x - (settlementSize / 2.0), y - (settlementSize / 2.0), settlementSize, settlementSize);
-    }
-
-    private void drawCity(double x, double y, double citySize, GraphicsContext mapCtx) {
-        mapCtx.fillRoundRect(x - (citySize / 2.0), y - (citySize / 2.0), citySize, citySize,
-                citySize / 2.0, citySize / 2.0);
     }
 }
