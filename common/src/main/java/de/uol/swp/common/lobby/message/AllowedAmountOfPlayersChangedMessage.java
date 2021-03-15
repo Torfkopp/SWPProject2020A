6500f5b99f3615f31fd0316d26ced14384d4debe
Aldin Dervisi <aldin.dervisi@uni-oldenburg.de>
2021-03-15 17:49:09 +0100

    Event Struktur aufgebaut für Pre-Game Einstellungen
    AllowedAmountOfPlayersChangedMessage aktualisiert im Hauptmenü die
    Lobbyliste.
    ChangeLobbySettingsRequest stellt eine Anfrage auf eine
    Veränderung der Lobbyeinstellungen.
    CreateLobbyResponse und JoinLobbyResponse haben nun das Lobby Objekt,
    um sofort auf dem neusten Stand zu sein.
    Für das LobbyUpdateEvent gilt das gleiche.
    UpdateLobbyMessage kümmert sich um die gesetzten Buttons in dem
    Lobbyfenster.
    
    Co-authored-by: Maximilian Lindner <maximilian.lindner@uni-oldenburg.de>

---
+++ common/src/main/java/de/uol/swp/common/lobby/message/AllowedAmountOfPlayersChangedMessage.java
@@ -0,0 +1,24 @@
+package de.uol.swp.common.lobby.message;
+
+import de.uol.swp.common.user.User;
+
+/**
+ * Message used to update the lobby list in the main menu.
+ *
+ * @author Maximilian Lindner
+ * @author Aldin Dervisi
+ * @see de.uol.swp.common.lobby.message.AbstractLobbyMessage
+ * @since 2021-03-14
+ */
+public class AllowedAmountOfPlayersChangedMessage extends AbstractLobbyMessage {
+
+    /**
+     * Constructor
+     *
+     * @param name Name of the lobby
+     * @param user User who updated the allowed player amount
+     */
+    public AllowedAmountOfPlayersChangedMessage(String name, User user) {
+        super(name, user);
+    }
+}
