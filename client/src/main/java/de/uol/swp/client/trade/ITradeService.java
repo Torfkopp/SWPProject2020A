afddeb2a0425b2e36848990771c29c9b3518bc1e
Phillip-André Suhr <phillip-andre.suhr@uni-oldenburg.de>
2021-04-08 00:46:48 +0200

    IGameService und ITradeService eingeführt: Teil 2
    Die Verantwortlichkeiten der Services auf Clientseite wurde verdeutlicht
    und Methoden entsprechend verlagert.
    
    - LOBBYSERVICE: Posten von Requests/Events in Bezug auf Lobbys direkt.
    - GAMESERVICE: Posten von Requests/Events in Bezug auf Games direkt.
    - TRADESERVICE: Posten von Requests/Events in Bezug auf Handelsereignisse
    direkt.
    
    Die Methoden des neuen ITradeService wurden entworfen und umgesetzt.
    
    Außerdem wurden die neuen Services in die verschiedenen Presenter
    eingepflegt, wo sie diverse eventBus.post()-Aufrufe ersetzen.
    
    Zusätzlich wurden die TradeWith*Presenter etwas aufgeräumt, was die
    Einführung der neuen Services erleichtert hat.
    
    Co-authored-by: Maximilian Lindner <maximilian.lindner@uni-oldenburg.de>

---
+++ client/src/main/java/de/uol/swp/client/trade/ITradeService.java
@@ -0,0 +1,154 @@
+package de.uol.swp.client.trade;
+
+import de.uol.swp.common.game.response.TradeWithUserOfferResponse;
+import de.uol.swp.common.user.User;
+import de.uol.swp.common.user.UserOrDummy;
+
+import java.util.Map;
+
+/**
+ * An interface for all methods of the TradeService
+ *
+ * @author Maximilian Lindner
+ * @author Phillip-André Suhr
+ * @see de.uol.swp.client.game.GameService
+ * @since 2021-04-07
+ */
+public interface ITradeService {
+
+    /**
+     * Posts a request to accept a proposed trade
+     *
+     * @param lobbyName         The name of the lobby
+     * @param respondingUser    The User who wants to accept the trade
+     * @param offeringUser      The User who offered the trade
+     * @param demandedResources The resources the offering User wants
+     * @param offeredResources  The resources the offering User is offering
+     */
+    void acceptUserTrade(String lobbyName, UserOrDummy respondingUser, UserOrDummy offeringUser,
+                         Map<String, Integer> demandedResources, Map<String, Integer> offeredResources);
+
+    /**
+     * Posts a request to buy a Development Card
+     *
+     * @param lobbyName The name of the lobby
+     * @param user      The user who wants to buy a Development Card
+     */
+    void buyDevelopmentCard(String lobbyName, User user);
+
+    /**
+     * Posts a request to close the Trade Accept window on the responding user's end
+     *
+     * @param lobbyName      The name of the lobby
+     * @param respondingUser The responding user (on whose end to close the window)
+     */
+    void cancelTrade(String lobbyName, UserOrDummy respondingUser);
+
+    /**
+     * Posts an event to close the Bank trade window and re-enable the in-game buttons
+     *
+     * @param lobbyName The name of the lobby
+     * @param user      The User who wants to close the Bank trade window
+     */
+    void closeBankTradeWindow(String lobbyName, User user);
+
+    /**
+     * Posts an event to close a Trade Response window
+     *
+     * @param lobbyName The name of the lobby
+     */
+    void closeTradeResponseWindow(String lobbyName);
+
+    /**
+     * Posts an event to close a User trade window
+     *
+     * @param lobbyName The name of the lobby
+     */
+    void closeUserTradeWindow(String lobbyName);
+
+    /**
+     * Posts a request to execute a resource trade with the Bank
+     *
+     * @param lobbyName      The name of the lobby
+     * @param user           The User who wants to trade with the Bank
+     * @param gainedResource The resource the User wants from the Bank
+     * @param lostResource   The resource the User is offering to the Bank
+     */
+    void executeTradeWithBank(String lobbyName, User user, String gainedResource, String lostResource);
+
+    /**
+     * Posts a request to offer a trade to another user
+     *
+     * @param lobbyName         The name of the lobby
+     * @param user              The user offering the trade
+     * @param respondingUser    The user to whom the trade offer is being made
+     * @param offeredResources  Map of resources being offered to the responding user
+     * @param demandedResources Map of resources being demanded from the responding user
+     */
+    void offerTrade(String lobbyName, UserOrDummy user, UserOrDummy respondingUser,
+                    Map<String, Integer> offeredResources, Map<String, Integer> demandedResources);
+
+    /**
+     * Posts a request to reset the Offer Trade button for the user who proposed a trade
+     * that got rejected
+     *
+     * @param lobbyName    The name of the lobby
+     * @param offeringUser The user whose trade offer was rejected
+     */
+    void resetOfferTradeButton(String lobbyName, UserOrDummy offeringUser);
+
+    /**
+     * Posts an event to show the Bank trade window
+     *
+     * @param lobbyName The name of the lobby
+     * @param user      The user who wants to trade with the Bank
+     */
+    void showBankTradeWindow(String lobbyName, User user);
+
+    /**
+     * Posts an event to show the Trade Offered window displaying a trade offer
+     * from another user
+     *
+     * @param lobbyName      The name of the lobby
+     * @param offeringUser   The user who offered a trade
+     * @param respondingUser The user receiving the trade offer
+     * @param rsp            The TradeWithUserOfferResponse containing the
+     *                       details of the offer
+     */
+    void showOfferWindow(String lobbyName, UserOrDummy offeringUser, User respondingUser,
+                         TradeWithUserOfferResponse rsp);
+
+    /**
+     * Posts an event to show a Trade Error alert with the provided message
+     *
+     * @param message The message to display
+     */
+    void showTradeError(String message);
+
+    /**
+     * Posts an event to show the User trade window for making a trade offer
+     *
+     * @param lobbyName      The name of the lobby
+     * @param offeringUser   The user wanting to create a trade offer
+     * @param respondingUser The user to whom the offer is being made
+     */
+    void showUserTradeWindow(String lobbyName, UserOrDummy offeringUser, UserOrDummy respondingUser);
+
+    /**
+     * Posts a request for the Bank's inventory in order to trade with the Bank
+     *
+     * @param lobbyName The name of the lobby
+     * @param user      The user who wants to trade with the Bank
+     */
+    void tradeWithBank(String lobbyName, User user);
+
+    /**
+     * Posts a request for another User's inventory overview in order to make a
+     * trade offer to them
+     *
+     * @param lobbyName      The name of the lobby
+     * @param offeringUser   The user who wants to make a trade offer
+     * @param respondingUser The user to whom the offer is being made
+     */
+    void tradeWithUser(String lobbyName, UserOrDummy offeringUser, UserOrDummy respondingUser);
+}
---
+++ client/src/main/java/de/uol/swp/client/trade/TradeService.java
@@ -0,0 +1,151 @@
+package de.uol.swp.client.trade;
+
+import com.google.common.eventbus.EventBus;
+import com.google.inject.Inject;
+import de.uol.swp.client.lobby.LobbyService;
+import de.uol.swp.client.trade.event.*;
+import de.uol.swp.common.game.request.*;
+import de.uol.swp.common.game.response.TradeWithUserOfferResponse;
+import de.uol.swp.common.message.Message;
+import de.uol.swp.common.user.User;
+import de.uol.swp.common.user.UserOrDummy;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Map;
+
+/**
+ * The TradeService is responsible for posting requests and events regarding
+ * trades between Users or a User and the Bank, like buying Development Cards
+ * or offering a trade.
+ *
+ * @author Maximilian Lindner
+ * @author Phillip-André Suhr
+ * @since 2021-04-07
+ */
+@SuppressWarnings("UnstableApiUsage")
+public class TradeService implements ITradeService {
+
+    private static final Logger LOG = LogManager.getLogger(LobbyService.class);
+    private final EventBus eventBus;
+
+    /**
+     * Constructor
+     *
+     * @param eventBus The EventBus set in ClientModule
+     *
+     * @see de.uol.swp.client.di.ClientModule
+     * @since 2021-04-07
+     */
+    @Inject
+    public TradeService(EventBus eventBus) {
+        this.eventBus = eventBus;
+        this.eventBus.register(this);
+        LOG.debug("GameService started");
+    }
+
+    @Override
+    public void acceptUserTrade(String lobbyName, UserOrDummy respondingUser, UserOrDummy offeringUser,
+                                Map<String, Integer> demandedResources, Map<String, Integer> offeredResources) {
+        LOG.debug("Sending AcceptUserTradeRequest");
+        Message request = new AcceptUserTradeRequest(respondingUser, offeringUser, lobbyName, demandedResources,
+                                                     offeredResources);
+        eventBus.post(request);
+    }
+
+    @Override
+    public void buyDevelopmentCard(String lobbyName, User user) {
+        LOG.debug("Sending BuyDevelopmentCardRequest");
+        Message buyDevelopmentCardRequest = new BuyDevelopmentCardRequest(user, lobbyName);
+        eventBus.post(buyDevelopmentCardRequest);
+    }
+
+    @Override
+    public void cancelTrade(String lobbyName, UserOrDummy respondingUser) {
+        LOG.debug("Sending TradeWithUserCancelRequest");
+        Message request = new TradeWithUserCancelRequest(lobbyName, respondingUser);
+        eventBus.post(request);
+    }
+
+    @Override
+    public void closeBankTradeWindow(String lobbyName, User user) {
+        LOG.debug("Sending TradeCancelEvent");
+        eventBus.post(new TradeCancelEvent(lobbyName));
+        LOG.debug("Sending ResetTradeWithBankButtonEvent");
+        eventBus.post(new ResetTradeWithBankButtonEvent(user, lobbyName));
+    }
+
+    @Override
+    public void closeTradeResponseWindow(String lobbyName) {
+        LOG.debug("Sending CloseTradeResponseEvent");
+        eventBus.post(new CloseTradeResponseEvent(lobbyName));
+    }
+
+    @Override
+    public void closeUserTradeWindow(String lobbyName) {
+        LOG.debug("Sending TradeCancelEvent");
+        eventBus.post(new TradeCancelEvent(lobbyName));
+    }
+
+    @Override
+    public void executeTradeWithBank(String lobbyName, User user, String gainedResource, String lostResource) {
+        LOG.debug("Sending ExecuteTradeWithBankRequest");
+        Message request = new ExecuteTradeWithBankRequest(user, lobbyName, gainedResource, lostResource);
+        eventBus.post(request);
+    }
+
+    @Override
+    public void offerTrade(String lobbyName, UserOrDummy user, UserOrDummy respondingUser,
+                           Map<String, Integer> offeredResources, Map<String, Integer> demandedResources) {
+        LOG.debug("Sending an OfferingTradeWithUserRequest");
+        Message request = new OfferingTradeWithUserRequest(user, respondingUser, lobbyName, offeredResources,
+                                                           demandedResources);
+        eventBus.post(request);
+    }
+
+    @Override
+    public void resetOfferTradeButton(String lobbyName, UserOrDummy offeringUser) {
+        LOG.debug("Sending ResetOfferTradeButtonRequest");
+        Message request = new ResetOfferTradeButtonRequest(lobbyName, offeringUser);
+        eventBus.post(request);
+    }
+
+    @Override
+    public void showBankTradeWindow(String lobbyName, User user) {
+        LOG.debug("Sending ShowTradeWithBankViewEvent");
+        eventBus.post(new ShowTradeWithBankViewEvent(user, lobbyName));
+    }
+
+    @Override
+    public void showOfferWindow(String lobbyName, UserOrDummy offeringUser, User respondingUser,
+                                TradeWithUserOfferResponse rsp) {
+        LOG.debug("Sending ShowTradeWithUserRespondViewEvent");
+        eventBus.post(new ShowTradeWithUserRespondViewEvent(rsp.getOfferingUser(), respondingUser, lobbyName, rsp));
+    }
+
+    @Override
+    public void showTradeError(String message) {
+        LOG.debug("Sending TradeErrorEvent");
+        eventBus.post(new TradeErrorEvent(message));
+    }
+
+    @Override
+    public void showUserTradeWindow(String lobbyName, UserOrDummy offeringUser, UserOrDummy respondingUser) {
+        LOG.debug("Sending ShowTradeWithUserViewEvent");
+        eventBus.post(new ShowTradeWithUserViewEvent(offeringUser, lobbyName, respondingUser));
+    }
+
+    @Override
+    public void tradeWithBank(String lobbyName, User user) {
+        LOG.debug("Sending TradeWithBankRequest");
+        Message request = new TradeWithBankRequest(lobbyName, user);
+        eventBus.post(request);
+    }
+
+    @Override
+    public void tradeWithUser(String lobbyName, UserOrDummy offeringUser, UserOrDummy respondingUser) {
+        LOG.debug("Sending a TradeWithUserRequest");
+        Message request = new TradeWithUserRequest(lobbyName, respondingUser, offeringUser);
+        eventBus.post(request);
+    }
+}
