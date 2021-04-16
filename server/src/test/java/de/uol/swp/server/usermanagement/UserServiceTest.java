a6dd078b257350e0cbee81cbd57929b8b78d95b6
Marvin Drees <marvin@ceres-sys.de>
2021-04-16 13:17:21 +0200

    UserServiceTest aufgeteilt
    userServiceTest und mehrere kleinere Tests aufgeteilt.
    
    Signed-off-by: Marvin Drees <marvin@ceres-sys.de>

--- server/src/test/java/de/uol/swp/server/usermanagement/UserServiceTest.java
+++ server/src/test/java/de/uol/swp/server/usermanagement/UserServiceTest.java
@@ -8,6 +8,7 @@ import de.uol.swp.common.user.request.DeleteUserRequest;
 import de.uol.swp.common.user.request.RegisterUserRequest;
 import de.uol.swp.common.user.request.UpdateUserAccountDetailsRequest;
 import de.uol.swp.server.usermanagement.store.H2BasedUserStore;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
@@ -23,26 +24,51 @@ class UserServiceTest {
     final UserManagement userManagement = new UserManagement(new H2BasedUserStore());
     final UserService userService = new UserService(bus, userManagement);
 
-    @Test
-    void userServiceTest() {
-        // Test registration
+    @BeforeEach
+    void registerUser() {
+        // Register user if not present, will have no effect on an already registered user
         final Message registerRequest = new RegisterUserRequest(user);
 
         // The post will lead to a call of a UserService function
         bus.post(registerRequest);
+    }
 
-        // can only test, if something in the state has changed
+    @Test
+    void registerUserTest() {
+        // We can only test, if something in the state has changed
         final User loggedInUser = userManagement.login(user.getUsername(), user.getPassword());
-
         assertNotNull(loggedInUser);
+
         // Cannot compare against the object or ID because RegisterUserRequest doesn't know which ID the user will get
         assertEquals(user.getUsername(), loggedInUser.getUsername());
         assertEquals(user.getEMail(), loggedInUser.getEMail());
+    }
+
+    @Test
+    void deleteUserTest() {
+        // We can only test, if something in the state has changed
+        final User loggedInUser = userManagement.login(user.getUsername(), user.getPassword());
+        assertNotNull(loggedInUser);
+
+        // Test deletion
+        final Message deletionRequest = new DeleteUserRequest(loggedInUser);
+
+        // The post will lead to a call of a UserService function
+        bus.post(deletionRequest);
+
+        // Test if user is deleted again
+        assertFalse(userManagement.getUser(user.getID()).isPresent());
+    }
+
+    @Test
+    void changeUserDetailsTest() {
+        // We can only test, if something in the state has changed
+        final User loggedInUser = userManagement.login(user.getUsername(), user.getPassword());
+        assertNotNull(loggedInUser);
 
         // Test changing details
         final User newUser = new UserDTO(loggedInUser.getID(), "Kakmir Indihos", "654321", "fourtwentytwo@blaze.it");
-        final Message changeAccountDetailsRequest = new UpdateUserAccountDetailsRequest(newUser,
-                                                                                        user.getPassword(),
+        final Message changeAccountDetailsRequest = new UpdateUserAccountDetailsRequest(newUser, user.getPassword(),
                                                                                         user.getUsername(),
                                                                                         user.getEMail());
 
@@ -53,14 +79,5 @@ class UserServiceTest {
         final Optional<User> changedUser = userManagement.getUser(loggedInUser.getID());
         assertEquals(newUser.getUsername(), changedUser.get().getUsername());
         assertEquals(newUser.getEMail(), changedUser.get().getEMail());
-
-        // Test deletion
-        final Message deletionRequest = new DeleteUserRequest(changedUser.get());
-
-        // The post will lead to a call of a UserService function
-        bus.post(deletionRequest);
-
-        // Test if user is deleted again
-        assertFalse(userManagement.getUser(user.getID()).isPresent());
     }
 }
