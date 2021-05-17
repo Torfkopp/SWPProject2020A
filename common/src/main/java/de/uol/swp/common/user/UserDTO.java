e4d1eac1365602edd9bc2cba761d3e3c998ded11
Aldin <aldin.dervisi@uni-oldenburg.de>
2021-05-17 17:27:42 +0200

    LengthRestriction for the Username
    The UserName is now restricted in the RegistrationPresenter to set
    the maximum amount of chars to 20.
    After reaching that amount the Username
    TextField will not go any further.
    
    In case the User manages to still enter more than
    20 chars of code, the UserDTO on the Server will
    throw an IllegalArgumentException.
    
    Author: Aldin Dervisi <aldin.dervisi@uni-oldenburg.de>

--- common/src/main/java/de/uol/swp/common/user/UserDTO.java
+++ common/src/main/java/de/uol/swp/common/user/UserDTO.java
@@ -18,6 +18,7 @@ public class UserDTO implements User {
     private final String username;
     private final String password;
     private final String eMail;
+    private final int usernameLength;
 
     /**
      * Constructor
@@ -30,10 +31,12 @@ public class UserDTO implements User {
     public UserDTO(int id, String username, String password, String eMail) {
         if (!Objects.nonNull(username)) throw new IllegalArgumentException("Username must not be null");
         if (!Objects.nonNull(password)) throw new IllegalArgumentException("Password must not be null");
+        if (!(username.length() < 21)) throw new IllegalArgumentException("Username can only be 20 symbols long");
         this.id = id;
         this.username = username;
         this.password = password;
         this.eMail = eMail;
+        this.usernameLength = username.length();
     }
 
     /**
@@ -108,4 +111,6 @@ public class UserDTO implements User {
     public String toString() {
         return getUsername();
     }
+
+    public int getUsernameLength() {return usernameLength;}
 }
